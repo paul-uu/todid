2201e8e993f363e2bd0e0165f6d17b217eb47c24
Paul <pwittschen@gmail.com>
2015-05-18 15:07:50 -0400

    Update main.js

--- main.js
+++ main.js
@@ -3,36 +3,26 @@ var app = app || {};
 
 	// ----------------------------------------------------------
 	// Date stuff:
-	var date = new Date();
-	var today_day = date.getDay(),
-		today_month = date.getMonth(),
-		today_date = date.getDate(),
-		today_year = date.getFullYear();
-
-	var day = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-	var month = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-
-	// date_display is simply a typical display of today's date
-	// date_short is to be used in defining the local storage key values
-	var date_display, date_short;
-	date_display = day[today_day] + ' ' + month[today_month] + ' ' + today_date + ', ' + today_year;
-	date_short = (add_zero(today_month + 1)) + '/' + (add_zero(today_date)) + '/' + today_year;
-
-	// put date into dom
-	$('#date').text(date_display);
-
-	// ensure 2-digits of month or day numerical values to maintain xx/xx/xxxx
-	function add_zero(int) {
-		if (int < 10) {
-			return '0' + int.toString();
-		} else { return int; }
+	Date.prototype.today = function () {  // today's date;
+	    return (((this.getMonth()+1) < 10) ? '0':'') + (this.getMonth()+1) +'/'+ ( (this.getDate() < 10) ? '0':'') + this.getDate() +'/'+ this.getFullYear();
 	}
+	Date.prototype.timeNow = function () {  // current time
+	    return ((this.getHours() < 10) ? '0':'') + this.getHours() +':'+ ((this.getMinutes() < 10) ? '0':'') + this.getMinutes();
+	}
+	var today = new Date();
+	var date = today.today(),
+		time = today.timeNow();
+	$('#date').text(date + ' - ' + time);
+	
 
-
-
+	// ----------------------------------------------------------
+	// Backbone stuff:
+	
 	// Model
 	var ToDid_Model = Backbone.Model.extend({
 		defaults: {
+			'date': date,
+			'time': time,
 			'read_title': '',
 			'read_url': '',
 			'read_tldr': '',
@@ -52,6 +42,7 @@ var app = app || {};
 
 	// ToDidModel View
 	var ToDid_View = Backbone.View.extend({
+		
 		tagName: 'div',
 		className: 'todid_item',
 		events: {
@@ -63,19 +54,22 @@ var app = app || {};
 			this.render();
 		},
 		render: function() {
-			var new_todid = this.$el.html(this.template(this.model));
-			$('#todid_listing').append(new_todid);
+			this.$el.html(this.template(this.model));
 			return this;
 		},
 		delete_entry: function() {
-
+			console.log('delete');
+			this.model.destroy();	// delete model
+			this.remove();			// delete view
 		}
 	});
 	// ToDid Collection View
 	var ToDids_View = Backbone.View.extend({
 		el: $('#page_container'),
 		events: {
-			'click #form_submit': 'add_entry'
+			'click #form_submit': 'add_entry',
+			'click #toggle_new': 'show_form',
+			'click #toggle_old': 'show_entries'
 		},
 		initialize: function() {
 			console.log('todid initialize');
@@ -84,7 +78,9 @@ var app = app || {};
 			this.collection.toJSON();
 
 			this.render();
+			// listen for changes to collection
 			this.collection.on('add', this.render_item, this);
+			this.collection.on('remove', this.remove_item, this);
 		},
 		render: function() {
 			var that = this;
@@ -94,15 +90,17 @@ var app = app || {};
 		},
 		render_item: function(model) {
 			var model_view = new ToDid_View({ model: model });
-			this.$el.append(model_view.render().el);
+			this.$('#todid_listing').append(model_view.render().el);
 		},
 		add_entry: function() {
-
+			console.log('add entry function');
 			var input = [];
 			$('.form_input').each(function(i) {
 				input.push($(this).val());
 			});
 			new_entry = new ToDid_Model({
+				'date': date,
+				'time': time,
 				'read_title': input[0],
 				'read_url': input[1],
 				'read_tldr': input[2],
@@ -116,6 +114,14 @@ var app = app || {};
 			this.collection.add(new_entry);
 			new_entry.save();
 			$('.form_input').val('');
+		},
+		show_form: function() {
+			$('#todid_listing').addClass('hidden');			
+			$('#form_container').removeClass('hidden');
+		},
+		show_entries: function() {
+			$('#form_container').addClass('hidden');			
+			$('#todid_listing').removeClass('hidden');
 		}
 	});
 	var form_view = new ToDids_View();
